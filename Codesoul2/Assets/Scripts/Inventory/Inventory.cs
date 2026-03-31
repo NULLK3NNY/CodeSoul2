4b3e9cf6535d8298b0f6ac07bd847d28ad28ae3c
Kenny <144939416+NULLK3NNY@users.noreply.github.com>
2026-03-31 23:52:27 +0100

    inv

--- Codesoul2/Assets/Scripts/Inventory/Inventory.cs
+++ Codesoul2/Assets/Scripts/Inventory/Inventory.cs
@@ -7,6 +7,32 @@ public class Inventory : MonoBehaviour
     public int maxSlots = 8;
     public List<InventoryItem> items = new();
 
+    // Inventory UI
+    public GameObject inventoryUI;
+    bool isInventoryOpen = false;
+
+    // Example item pickup and inventory toggle
+    public ItemData testItem;
+
+    private void Update()
+    {
+        ShowInventoryUI();
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            // Example item pickup
+            var itemToAdd = Resources.Load<ItemData>("ExampleItem");
+            if (AddItem(itemToAdd))
+            {
+                Debug.Log("Item added to inventory!");
+            }
+            else
+            {
+                Debug.Log("Inventory is full!");
+            }
+        }
+    }
+
     public bool AddItem(ItemData item, int amount = 1)
     {
         if (item.canStack)
@@ -29,5 +55,29 @@ public class Inventory : MonoBehaviour
         items.Add(new InventoryItem { data = item, quantity = amount });
 
         return true;
+    }
+
+    public void ShowInventoryUI()
+    {
+        // Open and close inventory
+        if (Input.GetKeyDown(KeyCode.Tab) && !isInventoryOpen)
+        {
+            isInventoryOpen = true;
+            inventoryUI.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && isInventoryOpen)
+        {
+            isInventoryOpen = false;
+            inventoryUI.SetActive(false);
+        }
+
+        // Show items
+        for (int i = 0; i < items.Count; i++)
+        {
+            var slot = inventoryUI.transform.GetChild(i);
+            var item = items[i];
+            slot.GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = item.data.itemSprite;
+            slot.GetChild(1).GetComponent<UnityEngine.UI.Text>().text = item.quantity > 1 ? item.quantity.ToString() : "";
+        }
     }
 }
